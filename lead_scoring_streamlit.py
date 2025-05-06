e0171246ea0dd8734df935bb84863ecd3f2a9b07
Pronob Gogoi <pronobgogoi356@gmail.com>
2025-05-06 08:04:23 +0530

    Added search bar

--- lead_scoring_streamlit.py
+++ lead_scoring_streamlit.py
@@ -4,6 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# set the visual
+st.set_page_config(layout="wide")
+
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
@@ -118,6 +121,10 @@ if __name__ == "__main__":
     # ✅ Apply the selected threshold
     y_prediction = (y_probability >= threshold).astype(int)
 
+    # accuracy score display
+    st.divider()
+    st.subheader("Accuracy of Prediction:")
+    st.code(accuracy_score(y_test, y_prediction))
     # display lead scores
     lead_scores = (y_probability * 100).round(2)
 
@@ -136,36 +143,48 @@ if __name__ == "__main__":
     # columns creation in streamlit
     st.subheader("🔍 Explore Sample & Original Data")
     st.markdown("Switch between the **two tabs below** to view **random lead scores** and their **original data**.")
-    tab1, tab2, tab3 = st.tabs(["10 RANDOM LIST AND THEIR PREDICTIONS", "THE LIST DETAILS", "GRAPHICAL REPRESENTATION"])
-    with tab1:
-        # Show the updated DataFrame with lead scores
+    tab1, tab2, tab3 = st.tabs(["LIST OF ALL THE PREDICTED CONVERSIONS", "THE LIST DETAILS", "GRAPHICAL REPRESENTATION"])
 
-        sample = df_with_scores.sample(10)
-        st.dataframe(sample[["lead_number", "actual_conversion", "predicted_lead_scores"]])
+    # create the threshold dataframe
+    converted = df_with_scores[df_with_scores["predicted_lead_scores"] >= threshold * 100]
+
+    # total conversions
+    st.subheader(f"Total conversions for threshold of {threshold}:")
+    st.code(len(converted))
+
+    # search bar
+    st.subheader("🔍Search By Lead Number:")
+    lead_number = st.number_input("Enter the Lead Number")
+    if st.button("Search"):
+        st.dataframe(df_with_scores[df_with_scores["lead_number"]==lead_number])
+
+    with tab1:
+        # Show the predicted conversion dataframe
+        st.dataframe(converted[["lead_number", "actual_conversion", "predicted_lead_scores"]])
 
     with tab2:
         # Extract lead numbers from the sample
-        lead_numbers_list = sample["lead_number"].tolist()
+        lead_numbers_list = converted["lead_number"].tolist()
 
         # Extract the lead numbers and their original indices
-        lead_numbers = sample["lead_number"]
-        sample_indices = sample.index
+        lead_numbers = converted["lead_number"]
+        converted_indices = converted.index
 
         # Use those indices to get rows from original df (this keeps the index and order)
 
-        df_original_matched = df.loc[sample_indices]
+        df_original_matched = df.loc[converted_indices]
         st.dataframe(df_original_matched)
     st.divider()
 
     with tab3:
         # graphically
         st.header("Graphically:")
-        # Showing the sample in the form of a scatter plot
+        # Showing the converted in the form of a scatter plot
         fig, ax = plt.subplots()
         fig.set_size_inches(12, 6)
 
         # Scatter plot with sample data, color-coded by Actual Conversion (0 = No, 1 = Yes)
-        scatter = ax.scatter(sample.index, sample["predicted_lead_scores"], c=sample["actual_conversion"], cmap='RdYlGn',
+        scatter = ax.scatter(x=df_with_scores.index, y=df_with_scores["predicted_lead_scores"], c=df_with_scores["actual_conversion"], cmap='RdYlGn',
                              s=100,
                              alpha=0.75)
 
@@ -183,16 +202,22 @@ if __name__ == "__main__":
         ax.axhline(y=threshold * 100, color='r', linestyle='--', label='Lead Score Threshold')
 
         # Adding the legend
-        ax.legend()
+        #ax.legend()
+        ax.legend(loc='best')
 
         # Enabling grid
         ax.grid(True)
 
         # Adjust layout for better visibility
         fig.tight_layout()
+        ax.set_xlabel("Threshold", labelpad=10)
+        ax.set_ylabel("Conversion %", labelpad=10)
+        ax.tick_params(axis='x', labelrotation=0)  # or 45
 
         # Display the plot in Streamlit
-        st.pyplot(fig)
+        #st.pyplot(fig)
+        ax.set_ylim(0, 100)
+        st.pyplot(fig, bbox_inches='tight')
 
     st.divider()
 
@@ -214,10 +239,6 @@ if __name__ == "__main__":
     st.pyplot(fig)
     st.markdown(f"#### ***ROC-AUC Score: {roc_auc_score(y_test, y_probability)}***")
 
-    # accuracy score
-    st.divider()
-    st.subheader("Accuracy Score:")
-    st.write(accuracy_score(y_test, y_prediction))
 
     # confusion matrix
     st.divider()
